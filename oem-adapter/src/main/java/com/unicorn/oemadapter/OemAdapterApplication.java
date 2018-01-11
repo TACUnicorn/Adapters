c4b16118936c937403cf90b4efb2c8d16be99d9b
Yang LI <zjzsliyang@gmail.com>
2018-01-11 10:13:17 +0800

    fix bugs

--- oem-adapter/src/main/java/com/unicorn/oemadapter/OemAdapterApplication.java
+++ oem-adapter/src/main/java/com/unicorn/oemadapter/OemAdapterApplication.java
@@ -2,8 +2,10 @@ package com.unicorn.oemadapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OemAdapterApplication {
 
 	public static void main(String[] args) {
